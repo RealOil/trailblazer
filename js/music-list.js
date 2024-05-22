b8c6e51ef20e7e056bc6a4e5a783174265201007
RealOil <chungyuj33@gmail.com>
2024-05-22 22:45:27 +0900

    All Update

---
+++ js/music-list.js
@@ -0,0 +1,79 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Star Rail",
+    artist: "HOYO-MiX",
+    img: "01.StarRail",
+    src: "01.StarRail"
+  },
+  {
+    name: "Space Walk",
+    artist: "HOYO-MiX",
+    img: "02.SpaceWalk",
+    src: "02.SpaceWalk"
+  },
+  {
+    name: "Flow Experience",
+    artist: "HOYO-MiX",
+    img: "11.FlowExperience",
+    src: "11.FlowExperience"
+  },
+  {
+    name: "Embers",
+    artist: "HOYO-MiX",
+    img: "01.Embers",
+    src: "01.Embers"
+  },
+  {
+    name: "Faded Sun",
+    artist: "HOYO-MiX",
+    img: "04.FadedSun",
+    src: "04.FadedSun"
+  },
+  {
+    name: "Sleep Tight",
+    artist: "HOYO-MiX",
+    img: "09.SleepTight",
+    src: "09.SleepTight"
+  },
+  {
+    name: "Wildfire",
+    artist: "HOYO-MiX",
+    img: "30.Wildfire",
+    src: "30.Wildfire"
+  },
+  {
+    name: "Lustrous Moonlight",
+    artist: "HOYO-MiX",
+    img: "02.LustrousMoonlight",
+    src: "02.LustrousMoonlight"
+  },
+  {
+    name: "Got a Date",
+    artist: "HOYO-MiX",
+    img: "16.GotaDate",
+    src: "16.GotaDate"
+  },
+  {
+    name: "A Dramatic Irony",
+    artist: "HOYO-MiX",
+    img: "20.ADramaticIrony",
+    src: "20.ADramaticIrony"
+  },
+  {
+    name: "The Jepella Rebellion",
+    artist: "HOYO-MiX",
+    img: "21.TheJepellaRebellion",
+    src: "21.TheJepellaRebellion"
+  },
+];
